d743055729f2a4629af8ee482746077bcd00d58d
Catalin Patrut <cpatrut@outlook.com>
2021-08-08 19:48:13 +0200

    deleting model mapper

--- src/main/java/dev/enblng/api/config/SwaggerConfig.java
+++ src/main/java/dev/enblng/api/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package dev.enblng.api.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,26 +11,14 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-//TODO: delete
 @Configuration
-public class BeanConfig {
+public class SwaggerConfig {
     private static final String SNAPSHOT = "1.0";
     private static final String HTTP_WWW_APACHE_ORG_LICENSES_LICENSE_2_0_HTML = "Usage of this code is forbidden without consent";
     private static final String APACHE_2_0 = "Apache 2.0";
 
     @Bean
-    public ModelMapper modelMapper() {
-        final ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
-        return modelMapper;
-    }
-
-    @Bean
     public Docket api() {
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
                 .select()
